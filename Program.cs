81c15f51f65c6f3f00ded5ecc4779003eb4276e5
ravij <ravij@LAPTOP-VFAM17U2>
2023-12-11 20:58:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace TestCodeConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            MyClass myInstance = new MyClass();
+            myInstance.MyEvent += MyEventHandler;
+            GenericClass<MyAZ> obj = new GenericClass<MyAZ>();
+        }
+
+
+        public delegate void MyDelegate(object sender, EventArgs e);
+
+        public class MyClass
+        {
+            public event MyDelegate MyEvent;
+        }
+
+        private void MyEventHandler(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //--------------  Generic class que
+
+
+
+
+        public class MyBase
+        { }
+
+        public class MyA : MyBase
+        { }
+
+        public class MyB
+        { }
+
+        public class MyC : MyB
+        { }
+
+        public class MyAZ
+        {
+            public MyAZ(MyB b, MyA a)
+            {
+            }
+
+            public MyAZ(MyC c, MyA a)
+            {
+            }
+        }
+        public class GenericClass<T> where T : new(MyC,MyA){ }
+        private ICollection<MyAZ> ts { get; set; }
+    }
+}
